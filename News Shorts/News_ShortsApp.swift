7a7732d6d8ef368e3aa551d267d732cba8f4ea2f
ASHISH <ashkh.goyal@gmail.com>
2021-08-18 11:25:36 +0530

    Initial Commit

---
+++ News Shorts/News_ShortsApp.swift
@@ -0,0 +1,17 @@
+//
+//  News_ShortsApp.swift
+//  News Shorts
+//
+//  Created by Ashish on 18/08/21.
+//
+
+import SwiftUI
+
+@main
+struct News_ShortsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
